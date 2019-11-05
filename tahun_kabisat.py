294b8b11051c1c09fc275ae4a5fe48785773ea8f
Daniel Kessie <41339834+Dankny@users.noreply.github.com>
2019-11-05 10:28:08 +0700

    Add files via upload

---
+++ tahun_kabisat.py
@@ -0,0 +1,7 @@
+#program menentukan tahun kabisat
+tahun = int(input('Masukkan tahun : ')
+
+if tahun / 4 == 0:
+    print('Tahun', str(tahun),' adalah tahun kabisat.)
+else:
+    print('Tahun', str(tahun,' adalah tahun biasa.)
